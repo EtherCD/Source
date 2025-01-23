ad9aaf407328f9b3f84d14dfb2e32494704b9052
EtherCD <ethernetcoder@gmail.com>
2025-01-23 19:11:14 +1300

    Trying to configure samba

--- modules/samba/default.nix
+++ modules/samba/default.nix
@@ -3,12 +3,23 @@
     enable = true;
     securityType = "user";
     openFirewall = true;
-    shares = {
-      "shared" = {
-        path = "/shared"; 
-        writable = true; 
-        browsable = false;
-        users = [ "ethercd" ];
+    settings = {
+      global = {
+        "workgroup" = "Umbriel";
+        "server string" = "smbnix";
+        "netbios name" = "smbnix";
+        "security" = "user";
+        "passwd program" = "/run/wrappers/bin/passwd %u";
+        "guest account" = "nobody";
+        "map to guest" = "bad user";
+      };
+      share = {
+        browseable = "yes";
+        comment = "Samba's server for my files";
+        "guest ok" = "no";
+        path = "/share";
+        "read only" = "no";
+        "writable" = "yes";
       };
     };
   };
